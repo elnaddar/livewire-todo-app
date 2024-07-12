608a6b666603eb442d04c0b1b4da30795bf59a31
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-12 06:40:43 +0300

    created todo list component

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', TodoList::class)->name('home');
